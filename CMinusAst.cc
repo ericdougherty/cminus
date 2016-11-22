7b264ae66415e2664b63fa970385113e72ea1b38
ericdougherty <ericdougherty@users.noreply.github.com>
2016-11-21 22:52:05 -0500

    Rough structure

--- CMinusAst.cc
+++ CMinusAst.cc
@@ -17,57 +17,155 @@ using std::endl;
 
 void
 PrintVisitor::visit (ProgramNode* node)
-{	
-	level = 0;
-	printLevel ();
-	cout << "Program" << endl;
-	++level;
-	for (auto child : node -> declarations)
-	{
-		child -> accept(this);
-	}
-	--level;
+{
+        level = 0;
+        printLevel ();
+        cout << "Program" << endl;
+        ++level;
+        for (auto child : node -> declarations)
+        {
+                child -> accept (this);
+        }
+        --level;
 }
 
 void
 PrintVisitor::visit (VariableDeclarationNode* node)
-{	
-	printLevel ();
-	printNodeInfo("Variable", node);
+{
+        printLevel ();
+        printNodeInfo ("Variable", node);
 }
 
 void
 PrintVisitor::visit (FunctionDeclarationNode* node)
-{	
-	printLevel ();
-	printNodeInfo("Function", node);
+{
+        printLevel ();
+        printNodeInfo ("Function", node);
 
-	++level;
-	for (auto child : node -> parameters)
-	{
-		child -> accept(this);
-	}
-	--level;
+        ++level;
+        for (auto child : node -> parameters)
+        {
+                child -> accept (this);
+        }
+        --level;
 }
 
 
 void
 PrintVisitor::visit (ParameterNode* node)
 {
-	printLevel ();
-	printNodeInfo("Parameter", node);
+        printLevel ();
+        printNodeInfo ("Parameter", node);
 
+}
+
+void
+PrintVisitor::visit (IfStatementNode* node)
+{
+        printLevel ();
+        cout << "If" << endl;
+        node -> conditionalExpression -> accept (this);
+        node -> thenStatement -> accept (this);
+        node -> elseStatement -> accept (this);
+}
+
+void
+PrintVisitor::visit (ForStatementNode* node)
+{
+        printLevel ();
+        cout << "For" << endl;
+        node -> initializer -> accept (this);
+        node -> condition -> accept (this);
+        node -> updater -> accept (this);
+        node -> body -> accept (this);
+}
+
+void
+PrintVisitor::visit (AssignmentExpressionNode* node)
+{
+        printLevel ();
+        node -> variable -> accept (this);
+        node -> expression -> accept (this);
+}
+
+void
+PrintVisitor::visit (VariableExpressionNode* node)
+{
+        printLevel ();
+        cout << node -> identifier << endl;
+}
+
+void
+PrintVisitor::visit (SubscriptExpressionNode* node)
+{
+        printLevel ();
+        node -> index -> accept (this);
+}
+
+void
+PrintVisitor::visit (AdditiveExpressionNode* node)
+{
+        printLevel ();
+        string op = "";
+        switch (node -> addOperator)
+        {
+            case AdditiveOperatorType::PLUS:
+                op = "+";
+                break;
+            case AdditiveOperatorType::MINUS:
+                op = "-";
+                break;
+        }
+        cout << op << endl;
+        node -> left -> accept (this);
+        node -> right -> accept (this);
+}
+
+void
+PrintVisitor::visit (RelationalExpressionNode* node)
+{
+        printLevel ();
+        string op = "";
+        switch (node -> relationalOperator)
+        {
+            case RelationalOperatorType::LT:
+                op = "<";
+                break;
+            case RelationalOperatorType::LTE:
+                op = "<=";
+                break;
+            case RelationalOperatorType::GT:
+                op = ">";
+                break;
+            case RelationalOperatorType::GTE:
+                op = ">=";
+                break;
+            case RelationalOperatorType::EQ:
+                op = "==";
+                break;
+            case RelationalOperatorType::NEQ:
+                op = "!=";
+                break;
+        }
+        cout << op << endl;
+        node -> left -> accept (this);
+        node -> right -> accept (this);
+}
+
+void
+PrintVisitor::visit (IntegerLiteralExpressionNode* node)
+{
+        printLevel ();
+        cout << node -> value << endl;
 }
 
 void
 PrintVisitor::printLevel ()
 {
-	for (size_t i = 0; i < level; ++i)
-	{
-		cout << "|   ";
-	}
-	
-
+        for (size_t i = 0; i < level; ++i)
+        {
+                cout << "|   ";
+        }
 }
 /********************************************************************/
 
@@ -77,37 +175,36 @@ Node::~Node ()
 /********************************************************************/
 
 ProgramNode::ProgramNode (vector<DeclarationNode*> declarations)
-	:declarations(declarations)
-	{}
+        :declarations (declarations)
+        {}
 
 
 ProgramNode::~ProgramNode()
 {
-	for (auto decl : declarations)
-		delete decl;
-	declarations.clear ();
+        for (auto decl : declarations)
+                delete decl;
+        declarations.clear ();
 }
 void
 ProgramNode::accept (IVisitor* visitor)
 {
-	visitor -> visit(this);
+        visitor -> visit (this);
 }
 
 /********************************************************************/
 
-
 DeclarationNode::DeclarationNode (ValueType t, string id)
-	:valueType(t), identifier(id), nestLevel(0)
-	{} 
+        :valueType (t), identifier (id), nestLevel (0)
+        {}
 
 DeclarationNode::~DeclarationNode ()
 {}
+
 /********************************************************************/
 
-
 VariableDeclarationNode::VariableDeclarationNode (ValueType t, string id)
-	:DeclarationNode(t, id)
-	{}
+        :DeclarationNode (t, id)
+        {}
 
 
 VariableDeclarationNode::~VariableDeclarationNode()
@@ -116,36 +213,35 @@ VariableDeclarationNode::~VariableDeclarationNode()
 void
 VariableDeclarationNode::accept (IVisitor* visitor)
 {
-	visitor -> visit(this);
+        visitor -> visit (this);
 }
 
 /********************************************************************/
 
-
-FunctionDeclarationNode::FunctionDeclarationNode (ValueType t, string id, 
-			    vector<ParameterNode*> params) //, CompoundStatementNode* body)
-	:DeclarationNode(t, id), parameters(params)//, functionBody(body)	
-	{}
+FunctionDeclarationNode::FunctionDeclarationNode (ValueType t, string id,
+                            vector<ParameterNode*> params, CompoundStatementNode* body)
+        :DeclarationNode (t, id), parameters (params), functionBody(body)
+        {}
 
 
 FunctionDeclarationNode::~FunctionDeclarationNode()
 {
-	for (auto param : parameters)
-		delete param;
-	parameters.clear ();
+        for (auto param : parameters)
+                delete param;
+        parameters.clear ();
 }
 
 void
 FunctionDeclarationNode::accept (IVisitor* visitor)
 {
-	visitor -> visit(this);
+        visitor -> visit (this);
 }
 
 /********************************************************************/
 
 ParameterNode::ParameterNode (ValueType t, string id, bool isArray)
-	:DeclarationNode(t, id), isArray (isArray)
-	{}
+        :DeclarationNode (t, id), isArray (isArray)
+        {}
 
 
 ParameterNode::~ParameterNode()
@@ -154,15 +250,171 @@ ParameterNode::~ParameterNode()
 void
 ParameterNode::accept (IVisitor* visitor)
 {
-	visitor -> visit(this);
+        visitor -> visit (this);
 }
 
 /********************************************************************/
+
+StatementNode::~StatementNode ()
+{}
+
+/********************************************************************/
+
+IfStatementNode::IfStatementNode (ExpressionNode* expr, StatementNode* thenStmt,
+                                StatementNode* elseStmt = nullptr)
+        :conditionalExpression(expr), thenStatement (thenStmt), elseStatement (elseStmt)
+{}
+
+IfStatementNode::~IfStatementNode ()
+{}
+
+void
+IfStatementNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
+ForStatementNode::ForStatementNode (ExpressionNode* e1,
+                    ExpressionNode* e2,
+                    ExpressionNode* e3,
+                    StatementNode* s)
+        :initializer (e1), condition (e2), updater (e3), body (s)
+{}
+
+ForStatementNode::~ForStatementNode ()
+{}
+
+void
+ForStatementNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
+ExpressionNode::~ExpressionNode ()
+{}
+
+/********************************************************************/
+
+ExpressionStatementNode::ExpressionStatementNode (ExpressionNode* expr)
+        :expression (expr)
+{}
+
+ExpressionStatementNode::~ExpressionStatementNode ()
+{}
+
+void
+ExpressionStatementNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
+AssignmentExpressionNode::AssignmentExpressionNode (VariableExpressionNode* var,
+                            ExpressionNode* expr)
+        :variable (var), expression (expr)
+{}
+
+AssignmentExpressionNode::~AssignmentExpressionNode ()
+{}
+
+void
+AssignmentExpressionNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
+VariableExpressionNode::VariableExpressionNode (string id)
+        :identifier (id)
+{}
+
+VariableExpressionNode::~VariableExpressionNode ()
+{}
+
+void
+VariableExpressionNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+/*
+SubscriptExpressionNode::SubscriptExpressionNode (string id, ExpressionNode* index)
+        :index (index)
+{}
+
+SubscriptExpressionNode::~SubscriptExpressionNode ()
+{}
+
+void
+SubscriptExpressionNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+*/
+/********************************************************************/
+
+AdditiveExpressionNode::AdditiveExpressionNode (AdditiveOperatorType addop,
+                          ExpressionNode* lhs,
+                          ExpressionNode* rhs)
+        :addOperator (addop), left (lhs), right (rhs)
+{}
+
+AdditiveExpressionNode::~AdditiveExpressionNode ()
+{}
+
+void
+AdditiveExpressionNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
+RelationalExpressionNode::RelationalExpressionNode (RelationalOperatorType relop,
+                          ExpressionNode* lhs,
+                          ExpressionNode* rhs)
+        :relationalOperator (relop), left (lhs), right (rhs)
+{}
+
+RelationalExpressionNode::~RelationalExpressionNode ()
+{}
+
+void
+RelationalExpressionNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
+IntegerLiteralExpressionNode::IntegerLiteralExpressionNode (int value)
+        :value (value)
+{}
+
+IntegerLiteralExpressionNode::~IntegerLiteralExpressionNode ()
+{}
+
+void
+IntegerLiteralExpressionNode::accept (IVisitor* visitor)
+{
+        visitor -> visit (this);
+}
+
+/********************************************************************/
+
 void
 printNodeInfo (string nodeType, auto node)
 {
-	cout << nodeType << ": " << node -> identifier << ": ";
-	string type = (node -> valueType == ValueType::INT) ? "int" : "void"; 
-	cout << type << " type" << endl;
+        cout << nodeType << ": " << node -> identifier << ": ";
+        string type = (node -> valueType == ValueType::INT) ? "int" : "void";
+        cout << type << " type" << endl;
 }
+
 /********************************************************************/
